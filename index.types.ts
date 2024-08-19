aa05e28fa03aad7a1be384dbe4577cdc54fc13a6
Francisco <public@francisco.io>
2024-08-19 21:29:41 +0200

    Added types

--- index.types.ts
+++ index.types.ts
@@ -1,35 +1,37 @@
 import umami from "./index.js";
 
-umami.view("/path");
-umami.event("/path");
+(async () => {
+  await umami.view("/path");
+  await umami.event("/path");
 
-await umami.view("/path", {
-  id: "abc",
-  ip: "abc",
-  agent: "abc",
-  tracker: "abc",
-  language: "abc",
-  referrer: "abc",
-  hostname: "abc",
-  screen: "abc",
-  data: { a: "b", c: "d" },
-});
+  await umami.view("/path", {
+    id: "abc",
+    ip: "abc",
+    agent: "abc",
+    tracker: "abc",
+    language: "abc",
+    referrer: "abc",
+    hostname: "abc",
+    screen: "abc",
+    data: { a: "b", c: "d" },
+  });
 
-await umami.event("/path", {
-  id: "abc",
-  ip: "abc",
-  agent: "abc",
-  tracker: "abc",
-  language: "abc",
-  referrer: "abc",
-  hostname: "abc",
-  screen: "abc",
-  data: { a: "b", c: "d" },
-});
+  await umami.event("/path", {
+    id: "abc",
+    ip: "abc",
+    agent: "abc",
+    tracker: "abc",
+    language: "abc",
+    referrer: "abc",
+    hostname: "abc",
+    screen: "abc",
+    data: { a: "b", c: "d" },
+  });
 
-umami.express({}, {}, () => {});
-umami.server({});
+  umami.express({}, {}, () => {});
+  umami.server({});
 
-umami.id = "abc";
-umami.tracker = "abc";
-umami.agent = "abc";
+  umami.id = "abc";
+  umami.tracker = "abc";
+  umami.agent = "abc";
+})();
